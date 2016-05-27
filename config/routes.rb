358b74de60c54fc528b45480b3bd540d41e082c6
Joe Bernardi <joebernardi@hotmail.com>
2016-05-26 22:35:55 -0500

    add microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -7,7 +8,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
 
+
+end
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
