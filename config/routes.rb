0c1f1dcf568590b0adc06f912ebe2bea6cd537cc
Joe Bernardi <joebernardi@hotmail.com>
2016-05-26 21:08:01 -0500

    add hello

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+def hello
 
+  render text: "Hello!"
+
+end
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
